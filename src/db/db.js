d6a24009d9a265842341d50806f21f248e184e17
rasheed <rasheedjawula18@gmail.com>
2025-09-18 14:35:43 +0000

    rafctored the db file to hide passkeys via .env file

--- src/db/db.js
+++ src/db/db.js
@@ -1,9 +1,11 @@
 import {Pool} from 'pg';
+import 'dotenv/config';
+
  export const pool = new Pool({
- 	user:"postgres",
- 	host:"localhost",
- 	database: "tasks_db",
- 	password:"rasheed",
+ 	user:process.env.DB_USER,
+ 	host:process.env.DB_HOST,
+ 	database: process.env.DB_DATABASE,
+ 	password:process.env.DB_PASSWORD,
  	port:5432
  }
  	);
